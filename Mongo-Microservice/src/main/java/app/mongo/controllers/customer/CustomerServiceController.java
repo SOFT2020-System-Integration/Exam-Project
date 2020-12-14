13cbfd7ca2c7bc7a62817934b4420fd678a2af93
Srax <thomas_ebsen@live.dk>
2020-12-14 15:41:35 +0100

    Customer Order

---
+++ Mongo-Microservice/src/main/java/app/mongo/controllers/customer/CustomerServiceController.java
@@ -0,0 +1,48 @@
+package app.mongo.controllers.customer;
+
+import app.mongo.exceptions.NotFoundException;
+import app.mongo.models.customer.Customer;
+import app.mongo.models.game.Game;
+import app.mongo.repositories.customer.CustomerService;
+import app.mongo.repositories.game.GameService;
+import com.mongodb.MongoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+//@RepositoryRestController
+@RepositoryRestResource
+@ResponseBody
+@RestController
+@RequestMapping("/customers")
+public class CustomerServiceController
+{
+    @Autowired
+    private CustomerService repo;
+
+    @GetMapping("/")
+    public List<Customer> retrieveAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public Optional<Customer> retrieveCustomerById(@PathVariable String id) throws NotFoundException, MongoException {
+        try {
+            return repo.findById(id);
+        } catch (MongoException ex) {
+            throw new NotFoundException(ex.getCode() + " : " + ex.getMessage());
+        }
+    }
+
+    @GetMapping("/mail/{title}")
+    public Customer retrieveCustomerByMail(@PathVariable String mail) throws NotFoundException, MongoException {
+        try {
+            return repo.findByMail(mail);
+        } catch (MongoException ex) {
+            throw new NotFoundException(ex.getCode() + " : " + ex.getMessage());
+        }
+    }
+}
---
+++ Mongo-Microservice/src/main/java/app/mongo/models/customer/Customer.java
@@ -0,0 +1,85 @@
+package app.mongo.models.customer;
+
+import app.mongo.models.game.GameType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.io.Serializable;
+
+@Data
+@Document(collection = "customers")
+public class Customer implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String firstName;
+    private String lastName;
+    @Indexed(unique = true, direction = IndexDirection.DESCENDING)
+    private String mail;
+    private String password;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName, String mail, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mail='" + mail + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
