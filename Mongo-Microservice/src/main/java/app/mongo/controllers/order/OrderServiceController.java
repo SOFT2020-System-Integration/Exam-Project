53808bec5e030ec754a127334358eca7c6d2a207
Srax <thomas_ebsen@live.dk>
2020-12-16 14:17:28 +0100

    We can now update orderlines status through the order

--- Mongo-Microservice/src/main/java/app/mongo/controllers/order/OrderServiceController.java
+++ Mongo-Microservice/src/main/java/app/mongo/controllers/order/OrderServiceController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 //@RepositoryRestController
 @RepositoryRestResource
@@ -66,7 +68,30 @@ public class OrderServiceController
                 _orderOpt.get().setStatus(status);
                 return orderRepo.save(_orderOpt.get());
             } else {
-                throw new MongoException("yeet?");
+                throw new MongoException("Something went wrong.");
+            }
+        } catch (MongoException ex) {
+            throw new NotFoundException(ex.getCode() + " : " + ex.getMessage());
+        }
+    }
+
+    @PutMapping("/id/{orderId}/orderlines/{orderlineId}/status/set/{status}")
+    public Order updateOrderLineStatusByOrderAndOrderLineId(@PathVariable String orderId, @PathVariable String orderlineId, @PathVariable Status status) throws MongoException {
+        Optional<Order> _orderOpt;
+        OrderLine orderLineToUpdate;
+        try {
+            _orderOpt = orderRepo.findById(orderId);
+            System.out.println(_orderOpt);
+            if(status.equals(Status.IN_PROGRESS) || status.equals(Status.COMPLETED)) {
+                List<OrderLine> list = _orderOpt.get().getOrderLines().stream()
+                        .filter(orderLine -> orderLine.getId().equals(orderlineId))
+                        .collect(Collectors.toList());
+                System.out.println(list);
+                orderLineToUpdate = list.get(0);
+                orderLineToUpdate.setStatus(status);
+                return orderRepo.save(_orderOpt.get());
+            } else {
+                throw new MongoException("Something went wrong.");
             }
         } catch (MongoException ex) {
             throw new NotFoundException(ex.getCode() + " : " + ex.getMessage());
