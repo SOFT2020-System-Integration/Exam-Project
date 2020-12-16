a3517b244badfc1e45ea35c63bf9ed9f3fdea047
Srax <thomas_ebsen@live.dk>
2020-12-16 11:46:46 +0100

    cleanup
    .

--- ApiGatewayold/src/main/java/app/controllers/shop/ShopController.java
+++
@@ -1,4 +0,0 @@
-package app.controllers.shop;
-
-public class ShopController {
-}
--- ApiGatewayoldold/src/main/java/app/models/game/Type.java
+++
@@ -1,8 +0,0 @@
-package app.models.game;
-
-import java.io.Serializable;
-
-public enum Type implements Serializable {
-    DIGITAL,
-    PHYSICAL
-}
